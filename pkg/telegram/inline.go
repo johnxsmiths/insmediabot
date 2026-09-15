35983a070dd9f52ad1b2c1e8ab8a3624f2e85f30
johnxsmiths <sortiqsolutions@proton.me>
2026-09-15 16:41:24 +0530

    fixed inline

--- pkg/telegram/inline.go
+++ pkg/telegram/inline.go
@@ -173,27 +173,5 @@ func (b *Bot) handleInlineQuery(ctx context.Context, iq *InlineQuery) error {
 		}
 	}
 
-	shortcode := resolver.ExtractShortcode(cleanURL)
-	deepLink := fmt.Sprintf("https://t.me/%s?start=dl_%s", botUser, shortcode)
-
-	articleCard := InlineQueryResultArticle{
-		Type:         "article",
-		ID:           "direct_card",
-		Title:        fmt.Sprintf("📥 Send Media via @%s", botUser),
-		Description:  "Tap here to get the high-speed video directly in chat",
-		ThumbnailURL: igLogoURL,
-		InputMessageContent: InputTextMessageContent{
-			MessageText: fmt.Sprintf("✨ <b>Download Ready!</b>\n🔗 <a href=\"%s\">Instagram Post</a>", cleanURL),
-			ParseMode:   "HTML",
-		},
-		ReplyMarkup: &InlineKeyboardMarkup{
-			InlineKeyboard: [][]InlineKeyboardButton{
-				{{Text: "⚡ Download in Bot", URL: deepLink}},
-				{{Text: "🔗 Instagram Link", URL: cleanURL}},
-			},
-		},
-	}
-	results = append([]interface{}{articleCard}, results...)
-
 	return b.client.AnswerInlineQuery(ctx, iq.ID, results, 300)
 }
